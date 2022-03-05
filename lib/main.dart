a71126121bec3886010d890b91c54e79ddcd4914
judsonpaiva <judson.paiva@hotmail.com>
2022-03-05 16:09:07 +0100

    Trabalhando com o Widget Text e Padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    Container(
-      color: Colors.red,
-    )
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Explorando Widgets"),
+        ),
+        body: myWidget(),
+      ),
+    );
+  }
+  Widget myWidget(){
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+          "Olá mundo Flutter!",
+      ),
+    );
+  }
 }
